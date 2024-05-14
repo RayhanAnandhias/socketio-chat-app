1bf8c7242426df63839e628e4b38c647978d75a6
RayhanAnandhias <rayhananandhias@gmail.com>
2024-05-14 13:47:01 +0700

    Init Project

---
+++ service/src/test/java/com/anandhias/socketapp/SocketappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anandhias.socketapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocketappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
